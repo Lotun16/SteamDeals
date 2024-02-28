4d5c909bd436a90588000991c474c03991660e38
Lotun16 <lotunkanjiojelade@yahoo.com>
2024-02-28 11:59:25 -0500

    image unavailable

--- src/components/GameImage.tsx
+++ src/components/GameImage.tsx
@@ -1,13 +1,23 @@
 import { VFC, useState } from "react";
+import imageUnavailable from '../images/imageUnavailable64.png';
 
 interface ImageProps {
     gameId: string;
 }
 const GameImage: VFC<ImageProps> = ({ gameId }) => {
     const [hasImage, setHasImage] = useState(true);
+    console.log('gameId: ', gameId)
 
     return (
-        !hasImage ? null :
+        !hasImage ? 
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '165px', border: '3px solid black', backgroundColor: 'black' }}>
+                <img
+                    alt="imageUn"
+                    src={imageUnavailable}
+                />
+            </div>
+
+            :
             <img
                 src={`https://dbxce1spal1df.cloudfront.net/${gameId}/banner400.jpg`}
                 style={{ height: '100%', width: 'auto' }}
